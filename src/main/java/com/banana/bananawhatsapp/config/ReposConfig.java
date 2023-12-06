2246ebc4dc05621d7e044bf7be24faf6523068ff
mibelcal <mibelcal@yahoo.es>
2023-12-06 11:42:13 +0100

    test conexión db

---
+++ src/main/java/com/banana/bananawhatsapp/config/ReposConfig.java
@@ -0,0 +1,45 @@
+package com.banana.bananawhatsapp.config;
+
+import com.banana.bananawhatsapp.*;
+import com.banana.bananawhatsapp.persistencia.DBConnector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class ReposConfig {
+
+//    @Value("${db_url}")
+//    String connUrl;
+//
+    @Bean
+    public DBConnector createDBConnector() {
+        return new DBConnector();
+    }
+//
+//    @Bean
+//    public IProductoRepository createIProductoRepository() {
+//        ProductoDBRepository repo = new ProductoDBRepository();
+//        repo.setConnUrl(connUrl);
+//        return repo;
+//    }
+//
+//    @Bean
+//    @Profile("default")
+//    public IUsuarioRepository createIUsuarioRepository() {
+//        System.out.println("usando UsuarioDBRepository...");
+//        UsuarioDBRepository repo = new UsuarioDBRepository();
+//        repo.setDb_url(connUrl);
+//        return repo;
+//    }
+//
+//    @Bean
+//    @Profile("dev")
+//    public IUsuarioRepository createInMemUsuarioRepository() {
+//        System.out.println("usando UsuarioInMemoryRepository...");
+//        UsuarioInMemoryRepository repo = new UsuarioInMemoryRepository();
+//        return repo;
+//    }
+
+}
