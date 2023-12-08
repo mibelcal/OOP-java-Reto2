bfbf4f692a973f80c2dc52ef1710c780070898e1
mibelcal <mibelcal@yahoo.es>
2023-12-08 10:31:38 +0100

    test crearusuariovalido UsuarioJDBCRepository

--- src/main/java/com/banana/bananawhatsapp/persistencia/UsuarioJDBCRepository.java
+++ src/main/java/com/banana/bananawhatsapp/persistencia/UsuarioJDBCRepository.java
@@ -1,19 +1,62 @@
 package com.banana.bananawhatsapp.persistencia;
 
+import com.banana.bananawhatsapp.exceptions.UsuarioException;
 import com.banana.bananawhatsapp.modelos.Usuario;
 import lombok.Setter;
 
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Set;
 
 @Setter
-public class UsuarioJDBCRepository implements IUsuarioRepository{
+public class UsuarioJDBCRepository implements IUsuarioRepository {
 
     private String db_url = null;
 
     @Override
     public Usuario crear(Usuario usuario) throws SQLException {
-        return null;
+        //IBC PTE DUDA ACTIVO BIT
+        String sql = "INSERT INTO usuario values (NULL,?,?,?,?)";
+        // String sql = "INSERT INTO usuario values (NULL,?,?,?,NULL)";
+
+        System.out.println("db_url: " + db_url);
+        try (
+                Connection conn = DriverManager.getConnection(db_url);
+                PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        ) {
+            if (usuario.valido() == true) {
+                System.out.println("Usuario válido!!!");
+
+                int valorInt = usuario.isActivo() ? 1 : 0;
+                System.out.println("valorInt: " + valorInt);
+
+                stmt.setBoolean(1, valorInt != 0);
+                stmt.setString(2, usuario.getAlta().toString());
+                stmt.setString(3, usuario.getEmail());
+                stmt.setString(4, usuario.getNombre());
+
+                int rows = stmt.executeUpdate();
+
+                ResultSet genKeys = stmt.getGeneratedKeys();
+                if (genKeys.next()) {
+                    usuario.setId(genKeys.getInt(1));
+                    System.out.println("Usuario creado correctamente!!!");
+                } else {
+                    throw new SQLException("Usuario creado erroneamente!!!");
+                }
+            }
+//            } else {
+//                throw new UsuarioException();
+//
+//            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new SQLException(e);
+        } catch (UsuarioException e) {
+            e.printStackTrace();
+            //throw new Exception(e); duda IBC
+        }
+
+        return usuario;
     }
 
     @Override
--- src/test/java/com/banana/bananawhatsapp/persistencia/UsuarioJDBCRepositoryTest.java
+++ src/test/java/com/banana/bananawhatsapp/persistencia/UsuarioJDBCRepositoryTest.java
@@ -1,13 +1,18 @@
 package com.banana.bananawhatsapp.persistencia;
 
 import com.banana.bananawhatsapp.config.SpringConfig;
+import com.banana.bananawhatsapp.modelos.Usuario;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.sql.SQLException;
+import java.time.LocalDate;
+
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.notNullValue;
 
 @ExtendWith(SpringExtension.class)
@@ -24,7 +29,15 @@ class UsuarioJDBCRepositoryTest {
     }
 
     @Test
-    void dadoUnUsuarioValido_cuandoCrear_entoncesUsuarioValido() {
+    void dadoUnUsuarioValido_cuandoCrear_entoncesUsuarioValido() throws SQLException {
+
+        Usuario user = new Usuario(null, "Isabel", "Isabel@gmail.com", LocalDate.of(2023, 12, 06), true);
+
+        repo.crear(user);
+
+        System.out.println(user);
+
+        assertThat(user.getId(), greaterThan(2));
     }
 
     @Test
