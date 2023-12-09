e65c528368edd5cc4e56e06d506c40f605f3d8d4
mibelcal <mibelcal@yahoo.es>
2023-12-09 13:13:33 +0100

    tests Alta OK y NOK ControladorUsuarios

--- src/test/java/com/banana/bananawhatsapp/controladores/ControladorUsuariosTest.java
+++ src/test/java/com/banana/bananawhatsapp/controladores/ControladorUsuariosTest.java
@@ -1,12 +1,19 @@
 package com.banana.bananawhatsapp.controladores;
 
 import com.banana.bananawhatsapp.config.SpringConfig;
+import com.banana.bananawhatsapp.exceptions.UsuarioException;
+import com.banana.bananawhatsapp.modelos.Usuario;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
@@ -22,11 +29,18 @@ class ControladorUsuariosTest {
     }
 
     @Test
-    void dadoUsuarioValido_cuandoAlta_entoncesUsuarioValido() {
+    void dadoUsuarioValido_cuandoAlta_entoncesUsuarioValido() throws SQLException {
+        Usuario usuario = controladorUsuarios.alta(new Usuario(null, "UsuControlador", "UsuControlador@hotmail.com", LocalDate.of(2023, 12, 8), true));
+        assertThat(usuario.getId(), greaterThan(5));
     }
 
     @Test
     void dadoUsuarioNOValido_cuandoAlta_entoncesExcepcion() {
+        assertThrows(UsuarioException.class, () -> {
+            //Usuario no válido: activo = false, debería ser true
+            Usuario user = new Usuario(null, "UsuControladorNOK", "UsuControladorNOK@hotmail.com", LocalDate.of(2023, 12, 9), false);
+            controladorUsuarios.alta(user);
+        });
     }
 
     @Test
