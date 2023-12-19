8aee3133203f983f20e5375d469f1eefdfbd2817
mibelcal <mibelcal@yahoo.es>
2023-12-19 10:22:10 +0100

    testBeans ControladorMensajesTest

--- src/test/java/com/banana/bananawhatsapp/controladores/ControladorMensajesTest.java
+++ src/test/java/com/banana/bananawhatsapp/controladores/ControladorMensajesTest.java
@@ -1,13 +1,26 @@
 package com.banana.bananawhatsapp.controladores;
 
+import com.banana.bananawhatsapp.config.SpringConfig;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {SpringConfig.class})
+
 class ControladorMensajesTest {
 
-
+    @Autowired
     ControladorMensajes controladorMensajes;
+
+    @Test
+    void testBeans() {
+        assertNotNull(controladorMensajes);
+    }
 
     @Test
     void dadoRemitenteYDestinatarioYTextoValidos_cuandoEnviarMensaje_entoncesOK() {
